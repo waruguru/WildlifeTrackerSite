795188b9d2dbff0c4d35d3dd7585694ea09f6384
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-13 19:37:59 +0300

    Add Database file

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,4 @@
+import java.sql.Connection;
 import java.util.List;
 
 public class Animal {
@@ -48,7 +49,7 @@ public class Animal {
     //saving a new animal by their name and type
     public void save() {
         String sql = "INSERT INTO animals (name, type) VALUES (:name, :type)";
-        try(Connection con = DB.sql2o.open()) {
+        try(Connection con = DataBase.sql2o.open()) {
             this.id = (int) con.createQuery(sql, true)
                     .addParameter("name", name)
                     .addParameter("type", type)
@@ -60,7 +61,7 @@ public class Animal {
 
     // deleting an animal and a sighting using their Id && throwing  exception incase the id is not mapped
     public void delete() {
-        try(Connection con = DB.sql2o.open()) {
+        try(Connection con = DataBase.sql2o.open()) {
             String sql = "DELETE from animals WHERE id = :id";
             con.createQuery(sql)
                     .addParameter("id", id)
@@ -78,7 +79,7 @@ public class Animal {
     public static List<Animal> all() {
         String sql = "SELECT * FROM animals;";
 
-        try (Connection con = DB.sql2o.open()) {
+        try (Connection con = DataBase.sql2o.open()) {
             return con.createQuery(sql)
                     .throwOnMappingFailure(false)
                     .executeAndFetch(Animal.class);
@@ -89,7 +90,7 @@ public class Animal {
     public static Animal find(int id) {
         String sql = "SELECT * FROM animals WHERE id = :id;";
 
-        try (Connection con = DB.sql2o.open()) {
+        try (Connection con = DataBase.sql2o.open()) {
             return con.createQuery(sql)
                     .addParameter("id", id)
                     .throwOnMappingFailure(false)
@@ -101,7 +102,7 @@ public class Animal {
     public void update() {
         String sql = "UPDATE animals SET name = :name WHERE id = :id";
 
-        try(Connection con = DB.sql2o.open()) {
+        try(Connection con = DataBase.sql2o.open()) {
             con.createQuery(sql)
                     .addParameter("name", name)
                     .addParameter("id", id)
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -7,7 +7,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "alpha", "pw*0711937973");
+        DataBase.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "alpha", "pw*0711937973");
     }
 
     @Override
@@ -15,7 +15,7 @@ public class DatabaseRule extends ExternalResource {
         String deleteAnimalsQuery = "DELETE FROM animals *;";
         String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
         String deleteSightingsQuery = "DELETE FROM sightings *;";
-        try(Connection con = DB.sql2o.open()) {
+        try(Connection con = DataBase.sql2o.open()) {
             con.createQuery(deleteAnimalsQuery).executeUpdate();
             con.createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
             con.createQuery(deleteSightingsQuery).executeUpdate();
