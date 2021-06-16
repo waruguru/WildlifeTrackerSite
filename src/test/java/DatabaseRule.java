578faebd18a130fcc6f2cf9411c9ae64a441be80
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-16 11:13:35 +0300

    Add readme file

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,22 +1,22 @@
-import org.junit.rules.ExternalResource;
-import org.sql2o.*;
-
-
-public class DatabaseRule extends ExternalResource {
-
-  @Override
-  protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:4567/wildlife_tracker", "moringa", "password");
-  }
-
-  @Override
-  protected void after() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteAnimalQuery = "DELETE FROM animals *;";
-      String deleteSightingsQuery = "DELETE FROM sightings *;";
-      con.createQuery(deleteAnimalQuery).executeUpdate();
-      con.createQuery(deleteSightingsQuery).executeUpdate();
-    }
-  }
-
-}
+//import org.junit.rules.ExternalResource;
+//import org.sql2o.*;
+//
+//
+//public class DatabaseRule extends ExternalResource {
+//
+//  @Override
+//  protected void before() {
+//    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:4567/wildlife_tracker", "moringa", "password");
+//  }
+//
+//  @Override
+//  protected void after() {
+//    try(Connection con = DB.sql2o.open()) {
+//      String deleteAnimalQuery = "DELETE FROM animals *;";
+//      String deleteSightingsQuery = "DELETE FROM sightings *;";
+//      con.createQuery(deleteAnimalQuery).executeUpdate();
+//      con.createQuery(deleteSightingsQuery).executeUpdate();
+//    }
+//  }
+//
+//}
